64b6131298f03d6eb129bac9fb178f4bf16a0a8a
van1ove <berezko2004@gmail.com>
2023-09-10 14:23:35 +0300

    added assets

---
+++ Assets/App/Scripts/Scenes/SceneFillwords/Features/ProviderLevel/ProviderFillwordLevel.cs
@@ -0,0 +1,18 @@
+using System;
+using App.Scripts.Scenes.SceneFillwords.Features.FillwordModels;
+
+namespace App.Scripts.Scenes.SceneFillwords.Features.ProviderLevel
+{
+    public class ProviderFillwordLevel : IProviderFillwordLevel
+    {
+        
+        
+        public GridFillWords LoadModel(int index)
+        {
+            //напиши реализацию не меняя сигнатуру функции
+            
+            
+            throw new NotImplementedException();
+        }
+    }
+}
