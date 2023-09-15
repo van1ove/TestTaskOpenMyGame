7f5c7650765dda3d2203e84d5a7c1417d92c5f20
van1ove <berezko2004@gmail.com>
2023-09-15 21:06:16 +0300

    Chess: working algo

--- Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/ChessGridNavigator.cs
+++ Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/ChessGridNavigator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using App.Scripts.Scenes.SceneChess.Features.ChessField.GridMatrix;
 using App.Scripts.Scenes.SceneChess.Features.ChessField.Types;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
@@ -12,8 +13,7 @@ namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
         public List<Vector2Int> FindPath(ChessUnitType unit, Vector2Int from, Vector2Int to, ChessGrid grid)
         {
             //напиши реализацию не меняя сигнатуру функции
-            List<Vector2Int> points = new List<Vector2Int>();
-
+            
             // structures initialization
             int[][] cellDistances = new int[grid.Size.x][];
             for (int i = 0; i < cellDistances.Length; i++)
@@ -48,57 +48,16 @@ namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
             }
             previous[from.x][from.y] = new Vector2Int(-1, -1);
 
-            // knight moveset 
-            int[] row = { 2, 2, -2, -2, 1, 1, -1, -1 };
-            int[] col = { -1, 1, 1, -1, 2, -2, 2, -2 };
-
-            // start of bfs
-            Queue<Vector2Int> queue = new Queue<Vector2Int>();
-            queue.Enqueue(from);
-
-            while (queue.Count > 0)
+            return unit switch
             {
-                Vector2Int currentCell = queue.Dequeue();
-                for (int i = 0; i < row.Length; i++)
-                {
-                    Vector2Int pos = new Vector2Int(currentCell.x + row[i], currentCell.y + col[i]);
-                    if (IsValid(pos, grid.Size) && !visited[pos.x][pos.y])
-                    {
-                        if (grid.Get(pos) != null) 
-                            continue;
-                        
-                        queue.Enqueue(pos);
-                        visited[currentCell.x][currentCell.y] = true;
-                        cellDistances[pos.x][pos.y] = cellDistances[currentCell.x][currentCell.y] + 1;
-                        previous[pos.x][pos.y] = currentCell;
-                    }
-                }
-                
-            }
-
-            Debug.Log(cellDistances[to.x][to.y]);
-            
-            if (cellDistances[to.x][to.y] == -1)
-                return null;
-            
-            // path recovery
-            points.Add(to);
-            Vector2Int current = to;
-            while (previous[current.x][current.y] != previous[from.x][from.y])
-            {
-                current = previous[current.x][current.y];
-                points.Add(current);
-                Debug.Log(points.Count);
-            }
-
-            points.Reverse();
-
-            return points;
-        }
-
-        private bool IsValid(Vector2Int position, Vector2Int gridSize)
-        {
-            return position.x >= 0 && position.x < gridSize.x && position.y >= 0 && position.y < gridSize.y;
+                ChessUnitType.Pon => UnitNavigator.PonNavigate(cellDistances, visited, previous, grid, from, to),
+                ChessUnitType.King => UnitNavigator.KingNavigate(cellDistances, visited, previous, grid, from, to),
+                ChessUnitType.Queen => UnitNavigator.QueenNavigate(cellDistances, visited, previous, grid, from, to),
+                ChessUnitType.Rook => UnitNavigator.RookNavigate(cellDistances, visited, previous, grid, from, to),
+                ChessUnitType.Knight => UnitNavigator.KnightNavigate(cellDistances, visited, previous, grid, from, to),
+                ChessUnitType.Bishop => UnitNavigator.BishopNavigate(cellDistances, visited, previous, grid, from, to),
+                _ => new List<Vector2Int> { from }
+            };
         }
     }
 }
---
+++ Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/UnitNavigator.cs
@@ -0,0 +1,337 @@
+﻿using System.Collections.Generic;
+using App.Scripts.Scenes.SceneChess.Features.ChessField.GridMatrix;
+using UnityEngine;
+
+namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
+{
+    public class UnitNavigator
+    {
+        public static List<Vector2Int> PonNavigate(int[][] cellDistances, bool[][] visited, Vector2Int[][] previous, 
+            ChessGrid grid, Vector2Int from, Vector2Int to)
+        {
+            List<Vector2Int> points = new List<Vector2Int>();
+
+            Vector2Int ponMove = new Vector2Int(0, 1);
+
+            // start of bfs
+            Queue<Vector2Int> queue = new Queue<Vector2Int>();
+            queue.Enqueue(from);
+
+            while (queue.Count > 0)
+            {
+                Vector2Int currentCell = queue.Dequeue();
+                Vector2Int pos = new Vector2Int(currentCell.x + ponMove.x, currentCell.y + ponMove.y);
+                
+                if (IsValid(pos, grid.Size) && !visited[pos.x][pos.y])
+                {
+                    if (grid.Get(pos) != null) 
+                        continue;
+                    
+                    queue.Enqueue(pos);
+                    visited[pos.x][pos.y] = true;
+                    cellDistances[pos.x][pos.y] = cellDistances[currentCell.x][currentCell.y] + 1;
+                    previous[pos.x][pos.y] = currentCell;
+                }
+
+            }
+            
+            if (cellDistances[to.x][to.y] == -1)
+                return null;
+            
+            // path recovery
+            points.Add(to);
+            Vector2Int current = to;
+            while (previous[current.x][current.y] != previous[from.x][from.y])
+            {
+                current = previous[current.x][current.y];
+                if (current == from) 
+                    continue;
+                
+                points.Add(current);
+            }
+
+            points.Reverse();
+            return points;
+        }
+        
+        public static List<Vector2Int> KingNavigate(int[][] cellDistances, bool[][] visited, Vector2Int[][] previous, 
+            ChessGrid grid, Vector2Int from, Vector2Int to)
+        {
+            List<Vector2Int> points = new List<Vector2Int>();
+            
+            int[] row = { 1, 1, 1, -1, -1, -1, 0, 0};
+            int[] col = { -1, 0, 1, -1, 0, 1, 1, -1};
+
+            // start of bfs
+            Queue<Vector2Int> queue = new Queue<Vector2Int>();
+            queue.Enqueue(from);
+
+            while (queue.Count > 0)
+            {
+                Vector2Int currentCell = queue.Dequeue();
+                for (int i = 0; i < row.Length; i++)
+                {
+                    Vector2Int pos = new Vector2Int(currentCell.x + row[i], currentCell.y + col[i]);
+                    if (IsValid(pos, grid.Size) && !visited[pos.x][pos.y])
+                    {
+                        if (grid.Get(pos) != null) 
+                            continue;
+                        
+                        queue.Enqueue(pos);
+                        visited[pos.x][pos.y] = true;
+                        cellDistances[pos.x][pos.y] = cellDistances[currentCell.x][currentCell.y] + 1;
+                        previous[pos.x][pos.y] = currentCell;
+                    }
+                }
+                
+            }
+            
+            if (cellDistances[to.x][to.y] == -1)
+                return null;
+            
+            // path recovery
+            points.Add(to);
+            Vector2Int current = to;
+            while (previous[current.x][current.y] != previous[from.x][from.y])
+            {
+                current = previous[current.x][current.y];
+                if (current == from) 
+                    continue;
+                
+                points.Add(current);
+            }
+            
+            points.Reverse();
+            return points;
+        }
+        
+        public static List<Vector2Int> QueenNavigate(int[][] cellDistances, bool[][] visited, Vector2Int[][] previous, 
+            ChessGrid grid, Vector2Int from, Vector2Int to)
+        {
+            List<Vector2Int> points = new List<Vector2Int>();
+            
+            int[] row = { 1, 1, 1, -1, -1, -1, 0, 0};
+            int[] col = { -1, 0, 1, -1, 0, 1, 1, -1};
+
+            // start of bfs
+            Queue<Vector2Int> queue = new Queue<Vector2Int>();
+            queue.Enqueue(from);
+
+            while (queue.Count > 0)
+            {
+                Vector2Int currentCell = queue.Dequeue();
+                for (int i = 0; i < row.Length; i++)
+                {
+                    for (int shift = 1; shift < grid.Size.x; shift++)
+                    {
+                        Vector2Int shiftedPos = ShiftPosition(row[i], col[i], shift);
+                        Vector2Int pos = new Vector2Int(currentCell.x + shiftedPos.x, currentCell.y + shiftedPos.y);
+
+                        if (IsValid(pos, grid.Size) && !visited[pos.x][pos.y])
+                        {
+                            if (grid.Get(pos) != null) 
+                                break;
+                        
+                            queue.Enqueue(pos);
+                            visited[pos.x][pos.y] = true;
+                            cellDistances[pos.x][pos.y] = cellDistances[currentCell.x][currentCell.y] + 1;
+                            previous[pos.x][pos.y] = currentCell;
+                        }
+                    }
+                }
+                
+            }
+            
+            if (cellDistances[to.x][to.y] == -1)
+                return null;
+            
+            // path recovery
+            points.Add(to);
+            Vector2Int current = to;
+            while (previous[current.x][current.y] != previous[from.x][from.y])
+            {
+                current = previous[current.x][current.y];
+                if (current == from) 
+                    continue;
+                
+                points.Add(current);
+            }
+
+            points.Reverse();
+            return points;
+        }
+        
+        public static List<Vector2Int> RookNavigate(int[][] cellDistances, bool[][] visited, Vector2Int[][] previous, 
+            ChessGrid grid, Vector2Int from, Vector2Int to)
+        {
+            List<Vector2Int> points = new List<Vector2Int>();
+            
+            int[] row = { 1, 0, -1, 0 };
+            int[] col = { 0, 1, 0, -1 };
+
+            // start of bfs
+            Queue<Vector2Int> queue = new Queue<Vector2Int>();
+            queue.Enqueue(from);
+
+            while (queue.Count > 0)
+            {
+                Vector2Int currentCell = queue.Dequeue();
+                for (int i = 0; i < row.Length; i++)
+                {
+                    for (int shift = 1; shift < grid.Size.x; shift++)
+                    {
+                        Vector2Int shiftedPos = ShiftPosition(row[i], col[i], shift);
+                        Vector2Int pos = new Vector2Int(currentCell.x + shiftedPos.x, currentCell.y + shiftedPos.y);
+
+                        if (IsValid(pos, grid.Size) && !visited[pos.x][pos.y])
+                        {
+                            if (grid.Get(pos) != null) 
+                                break;
+                        
+                            queue.Enqueue(pos);
+                            visited[pos.x][pos.y] = true;
+                            cellDistances[pos.x][pos.y] = cellDistances[currentCell.x][currentCell.y] + 1;
+                            previous[pos.x][pos.y] = currentCell;
+                        }
+                    }
+                }
+                
+            }
+            
+            if (cellDistances[to.x][to.y] == -1)
+                return null;
+            
+            // path recovery
+            points.Add(to);
+            Vector2Int current = to;
+            while (previous[current.x][current.y] != previous[from.x][from.y])
+            {
+                current = previous[current.x][current.y];
+                if (current == from) 
+                    continue;
+                
+                points.Add(current);
+            }
+
+            points.Reverse();
+            return points;
+        }
+        
+        public static List<Vector2Int> KnightNavigate(int[][] cellDistances, bool[][] visited, Vector2Int[][] previous, 
+            ChessGrid grid, Vector2Int from, Vector2Int to)
+        {
+            List<Vector2Int> points = new List<Vector2Int>();
+            
+            int[] row = { 2, 2, -2, -2, 1, 1, -1, -1 };
+            int[] col = { -1, 1, 1, -1, 2, -2, 2, -2 };
+
+            // start of bfs
+            Queue<Vector2Int> queue = new Queue<Vector2Int>();
+            queue.Enqueue(from);
+
+            while (queue.Count > 0)
+            {
+                Vector2Int currentCell = queue.Dequeue();
+                for (int i = 0; i < row.Length; i++)
+                {
+                    Vector2Int pos = new Vector2Int(currentCell.x + row[i], currentCell.y + col[i]);
+                    if (IsValid(pos, grid.Size) && !visited[pos.x][pos.y])
+                    {
+                        if (grid.Get(pos) != null) 
+                            continue;
+                        
+                        queue.Enqueue(pos);
+                        visited[pos.x][pos.y] = true;
+                        cellDistances[pos.x][pos.y] = cellDistances[currentCell.x][currentCell.y] + 1;
+                        previous[pos.x][pos.y] = currentCell;
+                    }
+                }
+                
+            }
+            
+            if (cellDistances[to.x][to.y] == -1)
+                return null;
+            
+            // path recovery
+            points.Add(to);
+            Vector2Int current = to;
+            while (previous[current.x][current.y] != previous[from.x][from.y])
+            {
+                current = previous[current.x][current.y];
+                if (current == from) 
+                    continue;
+                
+                points.Add(current);
+            }
+
+            points.Reverse();
+            return points;
+        }
+
+        public static List<Vector2Int> BishopNavigate(int[][] cellDistances, bool[][] visited, Vector2Int[][] previous, 
+            ChessGrid grid, Vector2Int from, Vector2Int to)
+        {
+            List<Vector2Int> points = new List<Vector2Int>();
+            
+            int[] row = { 1, 1, -1, -1 };
+            int[] col = { 1, -1, 1, -1 };
+
+            // start of bfs
+            Queue<Vector2Int> queue = new Queue<Vector2Int>();
+            queue.Enqueue(from);
+
+            while (queue.Count > 0)
+            {
+                Vector2Int currentCell = queue.Dequeue();
+                for (int i = 0; i < row.Length; i++)
+                {
+                    for (int shift = 1; shift < grid.Size.x; shift++)
+                    {
+                        Vector2Int shiftedPos = ShiftPosition(row[i], col[i], shift);
+                        Vector2Int pos = new Vector2Int(currentCell.x + shiftedPos.x, currentCell.y + shiftedPos.y);
+
+                        if (IsValid(pos, grid.Size) && !visited[pos.x][pos.y])
+                        {
+                            if (grid.Get(pos) != null) 
+                                break;
+                        
+                            queue.Enqueue(pos);
+                            visited[pos.x][pos.y] = true;
+                            cellDistances[pos.x][pos.y] = cellDistances[currentCell.x][currentCell.y] + 1;
+                            previous[pos.x][pos.y] = currentCell;
+                        }
+                    }
+                }
+                
+            }
+            
+            if (cellDistances[to.x][to.y] == -1)
+                return null;
+            
+            // path recovery
+            points.Add(to);
+            Vector2Int current = to;
+            while (previous[current.x][current.y] != previous[from.x][from.y])
+            {
+                current = previous[current.x][current.y];
+                if (current == from) 
+                    continue;
+                
+                points.Add(current);
+            }
+
+            points.Reverse();
+            return points;
+        }
+        
+        private static Vector2Int ShiftPosition(int x, int y, int shift)
+        {
+            return new Vector2Int(x * shift, y * shift);
+        }
+        
+        private static bool IsValid(Vector2Int position, Vector2Int gridSize)
+        {
+            return position.x >= 0 && position.x < gridSize.x && position.y >= 0 && position.y < gridSize.y;
+        }
+    }
+}
